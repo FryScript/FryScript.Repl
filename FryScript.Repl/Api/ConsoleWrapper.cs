bd61397be28b143dfbe79a14dfff3a798a2d5273
James Herring <james794251@hotmail.com>
2019-06-24 13:54:46 +0100

    Added initial REPL files

---
+++ FryScript.Repl/Api/ConsoleWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FryScript.Repl.Api
+{
+    [ScriptableType("console")]
+    public class ConsoleWrapper
+    {
+        [ScriptableMethod("writeLine")]
+        public void WriteLine(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        [ScriptableMethod("write")]
+        public void Write(string text)
+        {
+            Console.Write(text);
+        }
+    }
+}
---
+++ FryScript.Repl/Program.cs
@@ -0,0 +1,38 @@
+﻿using FryScript.Repl.Api;
+using System;
+
+namespace FryScript.Repl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fry Script Repl v1.0.0");
+
+            var runtime = new ScriptRuntime();
+            runtime.Import(typeof(ConsoleWrapper));
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("> ");
+
+                    var code = Console.ReadLine();
+
+                    var result = runtime.Eval(code);
+
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    var previous = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    Console.ForegroundColor = previous;
+                }
+            }
+        }
+    }
+}
